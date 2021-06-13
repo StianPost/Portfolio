7529fe97e13ecacd53404a7282bf33c5c120d623
Stian <69841774+StianPost@users.noreply.github.com>
2021-06-13 21:34:06 +0200

    Update projectList.js

--- js/projectList.js
+++ js/projectList.js
@@ -7,9 +7,6 @@ async function getProjects(url) {
   try {
     const response = await fetch(url);
     const result = await response.json();
-
-    console.log(result);
-
     getProjectCards(result);
   } catch (error) {
     document.querySelector('.alert').innerHTML = showAlertTouser(
